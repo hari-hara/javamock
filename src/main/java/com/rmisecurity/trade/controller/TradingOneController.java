f9d81a0badec072ecc5c9684ebc8472d54e4c00f
srao79@gmail.com <srao79@gmail.com>
2019-04-07 05:56:55 -0700

    commit for cross origin

--- src/main/java/com/rmisecurity/trade/controller/TradingOneController.java
+++ src/main/java/com/rmisecurity/trade/controller/TradingOneController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,8 +17,8 @@ import com.rmisecurity.trade.service.TradeOneService;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
 @EnableSwagger2
+@CrossOrigin
 @RestController
 public class TradingOneController {
 
